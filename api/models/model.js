d17b78c071719c382f137be3c3c2e8ea909c6ebe
Valentinakrennwallner <krennwallnervalentina@gmail.com>
2023-09-18 13:03:38 +0200

    v1

---
+++ api/models/model.js
@@ -0,0 +1,137 @@
+/* A class representing your resource. At the moment, its name is Resource. But you
+   can (and probalby should) rename it to whatever you are going to use as a Resource.
+   At the moment the example resource has only a name. You can delete this property
+   if you don't need it.
+
+   Task 1 - Part 1: Replace the Resource class with a new class of your choosing, that
+   has at least three properties: one string property, one number property, one boolean
+   property, and - optionally - a date property.
+   Then, adapt the initialization of the data at the end of this file (Task 2 - Part 2)
+   so that you have some instances of your object available that can be served to the client..
+ */
+class BookDiary {
+    constructor(name, author, description, genre, pages, rating, finished, isBorrowed) {
+        this.name = name;
+        this.author = author;
+        this.description = description;
+        this.genre = genre;
+        this.pages = pages;
+        this.rating = rating;
+        this.finished = finished;
+        this.isBorrowed = isBorrowed;
+    }
+}
+
+/* A model managing a map of resources. The id of the object is used as key in the map. */
+class Model {
+    static ID = 1;
+
+    constructor() {
+        this.resources = new Map();
+    }
+
+    add(resource) {
+        resource.id = Model.ID++;
+        this.resources.set(resource.id, resource);
+    }
+
+    get(id) {
+        this.checkId(id);
+        return this.resources.get(id);
+    }
+
+    getAll() {
+        return Array.from(this.resources.values());
+    }
+
+    checkId(id) {
+        if (typeof id !== "number") {
+            throw new Error(`Given id must be an number, but is a ${typeof id}`);
+        }
+    }
+
+    create(resource) {
+        this.add(resource);
+        return resource;
+    }
+
+    update(id, resource) {
+        this.checkId(id);
+
+        const target = this.resources.get(id);
+        if (!target) {
+            throw new Error(`Resource with ${id} does not exist and cannot be updated.`)
+        }
+
+        Object.assign(target, resource);
+
+        return target;
+    }
+
+    delete = (id) => {
+        this.checkId(id);
+        return this.resources.delete(id);
+    }
+}
+
+const model = new Model();
+
+/* Task 1 - Part 2. Replace these three instances of the example Class Resource with instances
+   of your own class */
+
+model.add(new BookDiary(
+    "Pet Sematary",
+    "Stephen King",
+    " \"Pet Sematary\" is a Stephen King novel about a family that moves to an idyllic country home, only to discover that a nearby Native American burial ground has the power to resurrect the dead. As tragedies befall the family, the father decides to harness this mystical force, leading to terrible consequences and blurring the boundaries between life and death. The book delves into the dark depths of human desire and the horrifying consequences of tampering with death.",
+    "Horror",
+    608,
+    5,
+    new Date("2023-08-05").toLocaleDateString(),
+    "false",
+));
+
+model.add(new BookDiary(
+    "The Soul Breaker",
+    "Sebastian Fitzek",
+    "\"The Soul Breaker\" incapacitates victims without killing them, leaving them in a catatonic state with a cryptic note. After a pause in abductions, a mysterious man is discovered near a remote psychiatric clinic, where the weather worsens, trapping everyone inside. As the clinic's head psychiatrist also falls victim to the Soul Breaker, it becomes clear that he has returned, leaving the clinic isolated from the outside world.",
+    "Thriller",
+    368,
+    4,
+    new Date("2023-07-15").toLocaleDateString(),
+    "false",
+));
+
+model.add(new BookDiary(
+    "Mind Gap",
+    "Anne Freytag",
+    "In a world on the brink of a transformative technological revolution, the NINK chip, initially designed to erase traumatic combat memories, raises profound questions. Silvie, a journalist, becomes ensnared in a reality-altering mystery when her brother is accused of a double murder and a suicide, and she embarks on a quest for answers. Her investigation unveils the dark potential of groundbreaking advancements when misused by malevolent forces.",
+    "Krimi",
+    384,
+    4,
+    new Date("2023-09-11").toLocaleDateString(),
+    "false",
+));
+
+model.add(new BookDiary(
+    "The Midnight Library",
+    "Matt Haig",
+    "Nora's life has been going from bad to worse. Then at the stroke of midnight on her last day on earth she finds herself transported to a library. There she is given the chance to undo her regrets and try out each of the other lives she might have lived. Which raises the ultimate question: with infinite choices, what is the best way to live?",
+    "Fantasy",
+    320,
+    4,
+    new Date("2023-04-24").toLocaleDateString(),
+    "false",
+));
+
+model.add(new BookDiary(
+    "Reminders of Him",
+    "Colleen Hoover",
+    "Five years after the tragic accident that claimed her great love Scott, Kenna returns to the scene with one goal: to reunite with her four-year-old daughter, Diem, who lives with Scott's parents. On her first evening back, she meets Ledger, the first man she's felt drawn to since Scott's passing, and their attraction is mutual. However, a revelation follows that Ledger was Scott's closest friend since childhood and has vowed that the unknown mother, whom he holds responsible for Scott's death, will never have a role in Diem's life.",
+    "Romance",
+    320,
+    3,
+    new Date("2023-06-29").toLocaleDateString(),
+    "true"
+));
+
+module.exports = model;
---
+++ files/main.js
@@ -0,0 +1,350 @@
+/* A builder class to simplify the task of creating HTML elements. */
+class ElementCreator {
+    constructor(tag) {
+        this.element = document.createElement(tag);
+    }
+
+    id(id) {
+        this.element.id = id;
+        return this;
+    }
+
+    class(clazz) {
+        this.element.class = clazz;
+        return this;
+    }
+
+    text(content) {
+        this.element.innerHTML = content;
+        return this;
+    }
+
+    with(name, value) {
+        this.element.setAttribute(name, value)
+        return this;
+    }
+
+    listener(name, listener) {
+        this.element.addEventListener(name, listener)
+        return this;
+    }
+
+    append(child) {
+        child.appendTo(this.element);
+        return this;
+    }
+
+    prependTo(parent) {
+        parent.prepend(this.element);
+        return this.element;
+    }
+
+    appendTo(parent) {
+        parent.append(this.element);
+        return this.element;
+    }
+
+    insertBefore(parent, sibling) {
+        parent.insertBefore(this.element, sibling);
+        return this.element;
+    }
+
+    replace(parent, sibling) {
+        parent.replaceChild(this.element, sibling);
+        return this.element;
+    }
+}
+
+/* A class representing a resource. This class is used per default when receiving the
+   available resources from the server (see end of this file).
+   You can (and probably should) rename this class to match with whatever name you
+   used for your resource on the server-side.
+*/
+class BookDiary {
+
+    /* If you want to know more about this form of getters, read this:
+     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get */
+    get idforDOM() {
+        return `resource-${this.id}`;
+    }
+}
+function add(resource, sibling) {
+    const creator = new ElementCreator("article")
+        .id(resource.idforDOM);
+
+    /* Task 2: Instead of the name property of the example resource, add the properties of
+       your resource to the DOM. If you do not have the name property in your resource,
+       start by removing the h2 element that currently represents the name. For the
+       properties of your object you can use whatever html element you feel represents
+       your data best, e.g., h2, paragraphs, spans, ...
+       Also, you don't have to use the ElementCreator if you don't want to and add the
+       elements manually. */
+
+    creator
+        .append(new ElementCreator("h2").text(resource.name + ' by ' + resource.author))
+    creator
+        .append(new ElementCreator("h3").text('Description: ' + resource.description))
+    creator
+        .append(new ElementCreator("p").text('Rating: ' + resource.rating + '/5'))
+    creator
+        .append(new ElementCreator("p").text('Pages: ' + resource.pages))
+    creator
+        .append(new ElementCreator("p").text('Genre: ' + resource.genre))
+    creator
+        .append(new ElementCreator("p").text('Finished on: ' + resource.finished))
+    creator
+        .append(new ElementCreator("p").text("Borrowed: " + resource.isBorrowed))
+
+    creator
+        .append(new ElementCreator("button").text("Edit").listener('click', () => {
+            edit(resource);
+        }))
+        .append(new ElementCreator("button").text("Remove").listener('click', () => {
+            /* Task 3: Call the delete endpoint asynchronously using either an XMLHttpRequest
+               or the Fetch API. Once the call returns successfully, remove the resource from
+               the DOM using the call to remove(...) below. */
+
+            fetch(`/api/resources/${resource.id}`, {method: "delete",})
+                .then(response => {
+                    if(response.ok) {
+                        console.log("Book successfully deleted.");
+                        remove(resource);  // <- This call removes the resource from the DOM. Call it after (and only if) your API call succeeds!
+                    }else{
+                        console.error("Error deleting the book.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Failed to delete the book.", error);
+                });
+        }));
+
+    const parent = document.querySelector('main');
+
+    if (sibling) {
+        creator.replace(parent, sibling);
+    } else {
+        creator.insertBefore(parent, document.querySelector('#bottom'));
+    }
+}
+
+function edit(resource) {
+    const formCreator = new ElementCreator("form")
+        .id(resource.idforDOM)
+        .append(new ElementCreator("h3").text("Edit " + resource.name));
+
+    /* Task 4 - Part 1: Instead of the name property, add the properties your resource has here!
+       The label and input element used here are just an example of how you can edit a
+       property of a resource, in the case of our example property name this is a label and an
+       input field. f, we assign the input field a unique id attribute to be able to identify
+       it easily later when the user saves the edited data (see Task 4 - Part 2 below).
+    */
+
+    formCreator
+        .append(new ElementCreator("label").text("Name").with("for", "resource-name"))
+        .append(new ElementCreator("input").id("resource-name").with("type", "text").with("value", resource.name));
+    formCreator
+        .append(new ElementCreator("label").text("Author").with("for", "resource-author"))
+        .append(new ElementCreator("input").id("resource-author").with("type", "text").with("value", resource.author));
+    formCreator
+        .append(new ElementCreator("label").text("Description").with("for", "resource-description"))
+        .append(new ElementCreator("input").id("resource-description").with("type", "text").with("value", resource.description));
+    formCreator
+        .append(new ElementCreator("label").text("Rating").with("for", "resource-rating"))
+        .append(new ElementCreator("input").id("resource-rating").with("type","number").with("value", resource.rating));
+    formCreator
+        .append(new ElementCreator("label").text("Pages").with("for", "resource-pages"))
+        .append(new ElementCreator("input").id("resource-pages").with("type", "number").with("value", resource.pages));
+    formCreator
+        .append(new ElementCreator("label").text("Genre").with("for", "resource-genre"))
+        .append(new ElementCreator("input").id("resource-genre").with("type", "text").with("value", resource.genre));
+    formCreator
+        .append(new ElementCreator("label").text("Finished").with("for", "resource-finished"))
+        .append(new ElementCreator("input").id("resource-finished").with("type", "date").with("value", resource.finished));
+    const originalYear = resource.finished; //needed if no new date selected to reset value
+
+    formCreator
+        .append(new ElementCreator("label").text("Borrowed").with("for", "resource-isBorrowed"))
+        .append(new ElementCreator("input").id("resource-isBorrowed").with("type", "checkbox"));
+
+
+    /* In the end, we add the code to handle saving the resource on the server and terminating edit mode */
+    formCreator
+        .append(new ElementCreator("button").text("Speichern").listener('click', (event) => {
+            /* Why do we have to prevent the default action? Try commenting this line. */
+            event.preventDefault();
+
+            /* The user saves the resource.
+               Task 4 - Part 2: We manually set the edited values from the input elements to the resource object.
+               Again, this code here is just an example of how the name of our example resource can be obtained
+               and set in to the resource. The idea is that you handle your own properties here.
+            */
+            resource.name = document.getElementById("resource-name").value;
+            resource.author = document.getElementById("resource-author").value
+            resource.description = document.getElementById("resource-description").value
+            resource.rating = document.getElementById("resource-rating").value
+            resource.pages = document.getElementById("resource-pages").value
+            resource.genre = document.getElementById("resource-genre").value
+
+            newfinished = document.getElementById("resource-finished").value;
+
+            var splitted = newfinished.split("-");
+
+            var years = splitted[0];                            //we split the date in order to reformat it into our format
+            var month = parseInt(splitted[1]);                            //System format YYYY-MM-DD
+            var day = parseInt(splitted[2]);                              //Our format DD.MM.YYYY
+
+            if (newfinished.length === 0){
+                resource.finished = originalYear;                               //We have to use the length option because using null or " " does not work here
+            }else{
+                resource.finished = day + "." + month + "." + years;            //If the user selected no date, the release status needs to keep its original date or else its empty
+            }
+
+            const checkBoxValue = document.getElementById(resource.idforDOM).querySelector('input[type="checkbox"]').checked;
+
+            checkBoxValue ? resource.isBorrowed = true : resource.isBorrowed = false;
+
+            /* Task 4 - Part 3: Call the update endpoint asynchronously. Once the call returns successfully,
+               use the code below to remove the form we used for editing and again render
+               the resource in the list.
+            */
+            fetch(`/api/resources/${resource.id}`, {
+                method: "put",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(resource)
+            })
+                .then(response => {
+                    if(response.ok) {
+                        console.log("Book successfully updated.");
+                        add(resource, document.getElementById(resource.idforDOM));
+                    }else{
+                        console.error("Error uploading the book.");
+                    }
+                })
+                .catch(error => {
+                    console.error("Failed to update the book", error);
+                });
+        }))
+        .replace(document.querySelector('main'), document.getElementById(resource.idforDOM));
+    const parentElement = document.getElementById(resource.idforDOM);
+    parentElement.querySelector('input[type="checkbox"]').checked = resource.isBorrowed;
+}
+
+function remove(resource) {
+    document.getElementById(resource.idforDOM).remove();
+}
+
+/* Task 5 - Create a new resource is very similar to updating a resource. First, you add
+   an empty form to the DOM with the exact same fields you used to edit a resource.
+   Instead of PUTing the resource to the server, you POST it and add the resource that
+   the server returns to the DOM (Remember, the resource returned by the server is the
+    one that contains an id).
+ */
+async function create() {
+    const book = new BookDiary();
+
+    const formCreator = new ElementCreator("form")
+        .id(book.idforDOM)
+        .append(new ElementCreator("h3").text("Add a new book"));
+
+    const fields = [
+        { label: "Name", value: "resource-name", type: "text" },
+        { label: "Author", id: "resource-author", type: "text" },
+        { label: "Description", id: "resource-description", type: "text" },
+        { label: "Rating", id: "resource-rating", type: "number" },
+        { label: "Pages", id: "resource-pages", type: "number" },
+        { label: "Genre", id: "resource-genre", type: "text" },
+        { label: "Finished", id: "resource-finished", type: "date" },
+        { label: "Borrowed", id: "resource-isBorrowed", type: "checkbox" }
+    ];
+
+    fields.forEach(field => {
+        formCreator
+            .append(new ElementCreator("label").text(field.label).with("for", field.id))
+            .append(new ElementCreator("input").id(field.id).with("type", field.type));
+    });
+
+    //---------- Add a save button to create the new resource ----------//
+    formCreator
+        .append(new ElementCreator("button").text("Save and Create").listener('click', async (event) => {
+            event.preventDefault();
+
+            //---------- Get values from the input fields ----------//
+
+            book.name = document.getElementById("resource-name").value;
+            book.author = document.getElementById("resource-author").value;
+            book.description = document.getElementById("resource-description").value;
+            book.rating = document.getElementById("resource-rating").value;
+            book.pages = document.getElementById("resource-pages").value;
+            book.genre = document.getElementById("resource-genre").value;
+
+            let finishedDate = document.getElementById("resource-finished").value;
+
+            if (finishedDate.length === 0) {
+                book.finished = null;
+            } else {
+                var split = finishedDate.split("-");
+
+                var year = split[0];
+                var month = parseInt(split[1]);
+                var day = parseInt(split[2]);
+
+                book.finished = day + "." + month + "." + year;
+            }
+            ;
+
+            const checkBoxValue = document.getElementById("resource-isBorrowed").checked;
+            book.isBorrowed = checkBoxValue;
+
+            const requestData = {
+                method: "POST",
+                headers: {                                      //This is our data for the POST request, we basically encapsulate it
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(book)
+            };
+
+            try {
+                const response = await fetch("/api/resources", requestData);
+                if (response.ok) {
+                    const createdResource = await response.json();
+                    add(Object.assign(new BookDiary(), createdResource));            //Here we make the post request to create the resource, add it to the DOM and then close the forms by removing it
+
+                    document.getElementById(book.idforDOM).remove();
+                } else {
+                    console.error("Error creating book.");
+                }
+            } catch (error) {
+                console.error("Failed to create book", error);
+            }
+            // ---------- Reset the input fields ----------//
+            fields.forEach(field => {
+                const element = document.getElementById(field.id);
+                if (element) {
+                    element.value = "";
+                    if (field.type === "checkbox") {
+                        element.checked = false;
+                    }
+                } else {
+                    console.error(`Element with ID ${field.id} not found in the DOM.`);
+                }
+            });
+        }))
+
+    //---------- Adding the form to the DOM ----------//
+    formCreator.appendTo(document.querySelector('main'));
+}
+
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    fetch("/api/resources")
+        .then(response => response.json())
+        .then(resources => {
+            for (const book of resources) {
+                add(Object.assign(new BookDiary(), book));
+
+            }
+        });
+});
